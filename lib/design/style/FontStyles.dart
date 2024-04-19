1fa10064cb8776eb897163d32445b07440f729aa
gitsuhyun <suhyun10201@naver.com>
2024-04-20 00:06:25 +0900

    [Design] 컬러 정의 추가

---
+++ lib/design/style/FontStyles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class FontStyles {
+  static const Title1 = TextStyle(
+      fontSize: 36,
+      fontWeight: FontWeight.w600,
+      wordSpacing: 48,
+      letterSpacing: -0.027,
+      fontFamily: 'Pretendard');
+  static const Title2 = TextStyle(
+      fontSize: 28,
+      fontWeight: FontWeight.w600,
+      wordSpacing: 48,
+      letterSpacing: -0.432,
+      fontFamily: 'Pretendard');
+}
