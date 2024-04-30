457a9e7ef2b2b978a62d00f2b1a17e9d45d4e8a2
jeong724 <oliveyou724@gmail.com>
2024-04-30 17:55:10 +0900

    [feat]news_letter

--- lib/view/home/news_letter_screen.dart
+++ lib/view/home/news_letter_screen.dart
@@ -1,25 +1,38 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:meetup/design/style/ColorStyles.dart';
+import 'package:meetup/design/style/FontStyles.dart';
 
 class NewsLetterScreen extends StatelessWidget{
-  const NewsLetterScreen({super.key});
+  const NewsLetterScreen({Key? key}) : super(key: key);
 
   @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('두둑'),
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16,0,0,0),
+              child: SvgPicture.asset('assets/images/newsletter_logo1.svg'),
+            ),
+            Expanded(
+              child: Container(),
+            ), // 공간을 채우기 위한 빈 컨테이너
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 16, 0), // 오른쪽에 여백 추가
+              child: SvgPicture.asset('assets/images/newsletter_call.svg'),
+            ),
+          ],
         ),
-        body: SingleChildScrollView(
-          child: Row(
-            children: [
-              SvgPicture.asset('assets/images/newsletter_logo.svg'),
-              Text('두둑'),
-            ],
-          ),
-        ),
-      );
-    }
-  }
+      ),
+      body: SingleChildScrollView(
+        child: Row(
+          children: [],
 
+        ),
+      ),
+    );
+  }
+}
