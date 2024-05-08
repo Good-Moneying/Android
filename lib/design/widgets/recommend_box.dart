9dd4c6df1650e9c5bfc201090fc8d3fe48910749
gitsuhyun <suhyun10201@naver.com>
2024-05-09 02:08:19 +0900

    [Design] 위젯 스타일 및 위치 수정

--- lib/design/widgets/recommend_box.dart
+++ lib/design/widgets/recommend_box.dart
@@ -1,82 +1,43 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
-
+import 'history_widget.dart';
 import '../style/ColorStyles.dart';
 import '../style/FontStyles.dart';
+import 'chip_editor.dart';
 
 class RecommendU extends StatelessWidget {
   const RecommendU({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 15),
-      child: SizedBox(
-        height: Get.height * 0.15,
+    return Card(
+      color: AppColors.white,
+      surfaceTintColor: Colors.transparent,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
         child: Row(
           children: [
             ClipRRect(
               borderRadius: BorderRadius.circular(10.0),
               child: Image.network(
                 'https://cdn.pixabay.com/photo/2016/03/23/15/00/ice-cream-1274894_1280.jpg',
-                height: Get.height * 0.15,
-                width: Get.width * 0.37,
+                height: Get.height * 0.13,
+                width: Get.width * 0.3,
                 fit: BoxFit.fill,
               ),
             ),
             Expanded(
-              child: Container(
-                padding: EdgeInsets.only(left: 13.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                        Chip(
-                          padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
-                          backgroundColor: AppColors.g1,
-                          shape: RoundedRectangleBorder(
-                            side: const BorderSide(
-                              color: Colors.transparent,
-                            ),
-                            borderRadius: BorderRadius.circular(50),
-                          ),
-                          label: Text(
-                            '코인',
-                            style: FontStyles.Caption2_m.copyWith(
-                                color: AppColors.g6),
-                          ),
-                        ),
-                        Spacer(),
-                        Icon(Icons.bookmark),
-                      ],
-                    ),
-                    Spacer(),
-                    Text(
-                      '“코인 급등 랠리?” 도지코인 거래 급감',
-                      style: FontStyles.Ln1_m.copyWith(
-                          color: AppColors.black),
-                      softWrap: true,
-                    ),
-                    Spacer(),
-                    Row(
-                      children: [
-                        SvgPicture.asset('assets/icons/history.svg'),
-                        SizedBox(
-                          width: Get.width*0.02,
-                        ),
-                        Text(
-                          '5분전',
-                          style: FontStyles.Caption2_r.copyWith(
-                              color: AppColors.g4),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+              child: Text(
+                '“코인 급등 랠리?” 도지코인 거래 급감',
+                style: FontStyles.Ln1_m.copyWith(color: AppColors.black),
+                softWrap: true,
               ),
             ),
+            SvgPicture.asset('assets/icons/bookmark_unfill.svg'),
           ],
         ),
       ),
--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:meetup/design/widgets/chip_editor.dart';
+import 'package:meetup/design/widgets/history_widget.dart';
 import 'package:meetup/design/widgets/recommend_box.dart';
 
 import '../../design/style/ColorStyles.dart';
@@ -53,7 +54,7 @@ class HomeScreen extends StatelessWidget {
                 style: FontStyles.Caption1_m.copyWith(color: AppColors.g4),
               ),
               SizedBox(
-                height: Get.height* 0.02,
+                height: Get.height * 0.02,
               ),
               //에디터 카드 위젯 만들기
               Card(
@@ -61,201 +62,194 @@ class HomeScreen extends StatelessWidget {
                 surfaceTintColor: Colors.transparent,
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(7)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(7),
-                        topRight: Radius.circular(7),
-                      ),
-                      //임시 사진
-                      child: Image.network(
-                        'https://cdn.pixabay.com/photo/2016/03/23/15/00/ice-cream-1274894_1280.jpg',
-                        height: Get.height * 0.2,
-                        width: Get.width,
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                    SizedBox(
-                      height: Get.height * 0.01,
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          CustomChip(label: '미국경제'),
-                          CustomChip(label: '금리'),
-                        ],
-                      ),
-                    ),
-                    ListTile(
-                      title: Text(
-                        '테슬라 주가 갑자기 오른 이유는?',
-                        style: FontStyles.Heading2_sb.copyWith(
-                            color: AppColors.black),
-                      ),
-                      subtitle: Text(
-                        '“코인 급등 랠리?” 도지코인 거래 급감',
-                        style:
-                            FontStyles.Caption1_m.copyWith(color: AppColors.g5),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(15, 0, 15, 15),
-                      child: OutlinedButton(
-                        onPressed: () {
-                          //에디터 화면 넘어가게하기
-                          Get.toNamed(Routes.EDITORNEWS);
-                        },
-                        style: OutlinedButton.styleFrom(
-                          backgroundColor: AppColors.white,
-                          side: BorderSide(
-                            color: AppColors.v2,
-                          ),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                        ),
-                        child: Text(
-                          '금융 지식 자세히 보기',
-                          style:
-                              FontStyles.Ln1_sb.copyWith(color: AppColors.v4),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              //에디터 카드
-              SizedBox(
-                height: Get.height * 0.03,
-              ),
-              Stack(
-                children: [
-                  Positioned.fill(
-                    child: Container(
-                      color: AppColors.white,
-                    ),
-                  ),
-                  Column(
+                elevation: 3,
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 20),
-                        child: Row(
-                          children: [
-                            RichText(
-                              text: TextSpan(
-                                children: [
-                                  //닉네임 들어갈 부분
-                                  TextSpan(
-                                    text: '두식이',
-                                    style: FontStyles.Headline2_b.copyWith(
-                                        color: AppColors.v6),
-                                  ),
-                                  TextSpan(
-                                    text: '님에게 추천해요!',
-                                    style: FontStyles.Headline2_b.copyWith(
-                                        color: AppColors.black),
-                                  )
-                                ],
-                              ),
-                            ),
-                            SizedBox(
-                              width: Get.width * 0.02,
-                            ),
-                            Tooltip(
-                              message: '관심있는 주제 및 나이, 성별에 따른 \n뉴스레터를 추천해드려요!',
-                              textStyle: FontStyles.Caption2_r.copyWith(
-                                  color: AppColors.white),
-                              decoration: ShapeDecoration(
-                                // borderRadius: BorderRadius.circular(8.0),
-                                color: AppColors.g4.withOpacity(0.95),
-                                shape: ToolTipBalloon(),
-                              ),
-                              triggerMode: TooltipTriggerMode.tap,
-                              child: SvgPicture.asset('assets/icons/info.svg'),
-                            ),
-                          ],
+                      ClipRRect(
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                        //임시 사진
+                        child: Image.network(
+                          'https://cdn.pixabay.com/photo/2016/03/23/15/00/ice-cream-1274894_1280.jpg',
+                          height: Get.height * 0.25,
+                          fit: BoxFit.fill,
                         ),
                       ),
                       SizedBox(
                         height: Get.height * 0.03,
                       ),
-                      RecommendU(),
-                      RecommendU(),
+                      Text(
+                        '테슬라 주가 갑자기 오른 이유는?',
+                        style: FontStyles.Headline2_b.copyWith(
+                            color: AppColors.black),
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          CustomChip(label: '미국경제'),
+                          CustomChip(label: '금리'),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 8.0, left: 10),
+                            child: History(),
+                          ),
+                        ],
+                      ),
                     ],
                   ),
-                ],
-              ),
-              SizedBox(
-                height: Get.height*0.02,
-              ),
-              Row(
-                children: [
-                  Text(
-                    '실시간 트렌드 뉴스',
-                    style:
-                        FontStyles.Headline2_b.copyWith(color: AppColors.black),
-                  ),
-                  SizedBox(
-                    width: Get.width * 0.02,
-                  ),
-                  Tooltip(
-                    message: '실시간 뉴스를 빠르게 \n핵심만 전달드릴게요!',
-                    textStyle:
-                        FontStyles.Caption2_r.copyWith(color: AppColors.white),
-                    decoration: ShapeDecoration(
-                      // borderRadius: BorderRadius.circular(8.0),
-                      color: AppColors.g4.withOpacity(0.95),
-                      shape: ToolTipBalloon(),
-                    ),
-                    triggerMode: TooltipTriggerMode.tap,
-                    child: SvgPicture.asset('assets/icons/info.svg'),
-                  ),
-                ],
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  //Get.toNamed(Routes.INTEREST);
-                },
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size.fromHeight(50),
-                  backgroundColor: AppColors.v5,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
                 ),
+              ),
+              //에디터 카드
+              SizedBox(
+                height: Get.height * 0.05,
+              ),
+              //실시간 트렌드 뉴스
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10.0),
                 child: Row(
                   children: [
-                    Text(
-                      '실시간 트렌드 더 알아보기',
-                      style: FontStyles.Bn2_m.copyWith(color: AppColors.white),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: '실시간 ',
+                            style: FontStyles.Headline2_b.copyWith(
+                                color: AppColors.v6),
+                          ),
+                          TextSpan(
+                            text: '트렌드 뉴스',
+                            style: FontStyles.Headline2_b.copyWith(
+                                color: AppColors.black),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: Get.width * 0.02,
+                    ),
+                    Tooltip(
+                      message: '실시간 뉴스를 빠르게 \n핵심만 전달드릴게요!',
+                      textStyle: FontStyles.Caption2_r.copyWith(
+                          color: AppColors.white),
+                      decoration: ShapeDecoration(
+                        // borderRadius: BorderRadius.circular(8.0),
+                        color: AppColors.g4.withOpacity(0.95),
+                        shape: ToolTipBalloon(),
+                      ),
+                      triggerMode: TooltipTriggerMode.tap,
+                      child: SvgPicture.asset('assets/icons/info.svg'),
                     ),
                     Spacer(),
-                    SvgPicture.asset('assets/icons/arrow_right.svg'),
+                    Text(
+                      '전체보기',
+                      style:
+                          FontStyles.Caption1_m.copyWith(color: AppColors.g4),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 7.0),
+                      child: SvgPicture.asset('assets/icons/view_more.svg'),
+                    ),
                   ],
                 ),
               ),
-              Text(
-                '오늘의 단어',
-                style: FontStyles.Headline2_b.copyWith(color: AppColors.black),
+
+              //추천 뉴스
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10.0),
+                child: Row(
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          //닉네임 들어갈 부분
+                          TextSpan(
+                            text: '두식이',
+                            style: FontStyles.Headline2_b.copyWith(
+                                color: AppColors.v6),
+                          ),
+                          TextSpan(
+                            text: '님에게 추천해요!',
+                            style: FontStyles.Headline2_b.copyWith(
+                                color: AppColors.black),
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      width: Get.width * 0.02,
+                    ),
+                    Tooltip(
+                      message: '관심있는 주제 및 나이, 성별에 따른 \n뉴스레터를 추천해드려요!',
+                      textStyle: FontStyles.Caption2_r.copyWith(
+                          color: AppColors.white),
+                      decoration: ShapeDecoration(
+                        // borderRadius: BorderRadius.circular(8.0),
+                        color: AppColors.g4.withOpacity(0.95),
+                        shape: ToolTipBalloon(),
+                      ),
+                      triggerMode: TooltipTriggerMode.tap,
+                      child: SvgPicture.asset('assets/icons/info.svg'),
+                    ),
+                  ],
+                ),
+              ),
+              RecommendU(),
+              RecommendU(),
+              SizedBox(
+                height: Get.height * 0.02,
+              ),
+              //오늘의 단어
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10.0),
+                child: Row(
+                  children: [
+                    Text(
+                      '오늘의 단어',
+                      style: FontStyles.Headline2_b.copyWith(
+                          color: AppColors.black),
+                    ),
+                    Spacer(),
+                    Text(
+                      '전체보기',
+                      style:
+                          FontStyles.Caption1_m.copyWith(color: AppColors.g4),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 7.0),
+                      child: SvgPicture.asset('assets/icons/view_more.svg'),
+                    ),
+                  ],
+                ),
               ),
               Card(
                 color: AppColors.white,
                 surfaceTintColor: Colors.transparent,
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(7)),
+                  borderRadius: BorderRadius.circular(10),
+                ),
                 child: Padding(
-                  padding: const EdgeInsets.all(18.0),
+                  padding: const EdgeInsets.all(15),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      //단어
+                      Row(
+                        children: [
+                          Text(
+                            '테슬라 요건',
+                            style:
+                                FontStyles.Bn1_b.copyWith(color: AppColors.v7),
+                          ),
+                          CustomChip(label: '기업'),
+                          Spacer(),
+                          SvgPicture.asset('assets/icons/bookmark_unfill.svg'),
+                        ],
+                      ),
                       Text(
-                        '테슬라 요건',
-                        style: FontStyles.Bn1_b.copyWith(color: AppColors.v7),
+                        'Tesla’s requirements',
+                        style: FontStyles.Ln1_m.copyWith(color: AppColors.g3),
                       ),
                       SizedBox(
                         height: Get.height * 0.01,
@@ -267,8 +261,7 @@ class HomeScreen extends StatelessWidget {
                         text: TextSpan(
                           text:
                               '주식 시장에 상장하기 위한 요건을 갖추기 못한 기업들 중에서 잠재력이 큰 곳들을 골라 상장 기회를 주는 제도에요!',
-                          style: FontStyles.Caption1_m.copyWith(
-                              color: AppColors.g6),
+                          style: FontStyles.Bn1_r.copyWith(color: AppColors.g5),
                         ),
                       ),
                     ],
