838360d236367641d76c910fa8661b0739bf9970
jeong724 <oliveyou724@gmail.com>
2024-05-08 17:12:04 +0900

    [feat] 마이페이지 ui 앱바, 이미지 추가

--- lib/view/mypage/profile_screen.dart
+++ lib/view/mypage/profile_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:meetup/design/style/ColorStyles.dart';
+import 'package:meetup/design/style/FontStyles.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -6,13 +10,54 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('마이페이지 화면입니다.'),
+      appBar: AppBar(backgroundColor: Colors.white,
+        title: Row(
+          children: [
+            Text('마이페이지',style: FontStyles.Headline2_b.copyWith(color: AppColors.g6),),
+          Expanded(
+            child: Container(),
+          )
+            ,SvgPicture.asset('assets/images/mypage_setting.svg')
+          ],
+        ),
       ),
       body: SingleChildScrollView(
           child: Column(
         children: [
-          Text('마이페이지'),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0,12,0,0),
+                child: Image.asset('assets/images/mypage_profile.png',width: 54,height: 54,),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 12.0),
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 14.0),
+                  child: Column(
+                    children: [
+                      Container(width: 40,height: 18,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(50),
+                          color: AppColors.y5,
+                        ),
+                        child: Center(
+                          child: Text(
+                            'LV. 1',
+                            style: FontStyles.Caption1_sb.copyWith(color: AppColors.g1),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 4.0),
+                        child: Text('김두둑'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       )),
     );
