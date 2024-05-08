b1838b2d42a5e0a850c3a5f595874a99d87094b8
jeong724 <oliveyou724@gmail.com>
2024-05-08 19:11:02 +0900

    [feat] 목표 달성률

--- lib/view/mypage/profile_screen.dart
+++ lib/view/mypage/profile_screen.dart
@@ -60,26 +60,27 @@ class ProfileScreen extends StatelessWidget {
                             ),
                           ],
                         ),
-                          SizedBox(width: 150,),
-                          SizedBox(
-                                child: Container(width: 89,height: 47,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(32),
-                                  color: Colors.white
-                                ),
-                                child: Center(
-                                  child: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Image.asset('assets/images/mypage_dotoricoin 1.png',width: 29,height: 31,),
-                                        SizedBox(width: 8,)
-                                        ,Text('12',style: FontStyles.Heading1_sb.copyWith(color: AppColors.g6)
-                                        ),
-                                    ],
+                            SizedBox(width: 150,),
+                            SizedBox(
+                                  child: Container(width: 89,height: 47,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(32),
+                                    color: Colors.white
                                   ),
-                                ),
+                                  child: Center(
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: [
+                                        Image.asset('assets/images/mypage_dotoricoin 1.png',width: 29,height: 31,),
+                                          SizedBox(width: 8,)
+                                          ,Text('12',style: FontStyles.Heading1_sb.copyWith(color: AppColors.g6)
+                                          ),
+                                      ],
+                                    ),
                                   ),
-                                                      ),
+
+                                                        ),
+                          ),
                         SizedBox(width: 16,),
                       ],
                     ),
@@ -88,21 +89,50 @@ class ProfileScreen extends StatelessWidget {
                 ),
               ],
             ),
-                  Container(width: 328,height: 172,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: Colors.white
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 16.0),
-                            child: Text('목표 달성률',style: FontStyles.Br2_sb.copyWith(color: Colors.black),),
-                          )
-                        ],
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Container(width: 328,height: 172,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 16.0),
+                              child: Text('목표 달성률',style: FontStyles.Br2_sb.copyWith(color: Colors.black),),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 4.0),
+                              child: RichText(text: TextSpan(
+                                text: '일주일 중에 ', style: FontStyles.Reading_m.copyWith(color: AppColors.g5),
+                                children: <TextSpan>[
+                                  TextSpan(text: '3',style: FontStyles.Br1_sb.copyWith(color: AppColors.v5),//기본style을 지정해줘야함
+                                  ),
+                                  TextSpan(text: '일동안 뉴스를 읽었어요!', style: FontStyles.Reading_m.copyWith(color: AppColors.g5))
+                                ],
+                              )),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 12.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  Text('일',style: FontStyles.Label1_Normal_m.copyWith(color: AppColors.g4),),
+                                  Text('월',style: FontStyles.Label1_Normal_m.copyWith(color: AppColors.g4),),
+                                  Text('화',style: FontStyles.Label1_Normal_m.copyWith(color: AppColors.g4),),
+                                  Text('수',style: FontStyles.Label1_Normal_m.copyWith(color: AppColors.g4),),
+                                  Text('목',style: FontStyles.Label1_Normal_m.copyWith(color: AppColors.g4),),
+                                  Text('금',style: FontStyles.Label1_Normal_m.copyWith(color: AppColors.g4),),
+                                  Text('토',style: FontStyles.Label1_Normal_m.copyWith(color: AppColors.g4),),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   )
