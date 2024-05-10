b55e5096b15e8cab5152ac2de535ae6d3272423c
gitsuhyun <suhyun10201@naver.com>
2024-05-10 17:15:21 +0900

    [Remove] 웹 로그인 방식 파일 삭제

--- lib/routes/get_pages.dart
+++ lib/routes/get_pages.dart
@@ -1,11 +1,9 @@
 import 'package:get/get.dart';
 
-import 'package:meetup/service/auth_service.dart';
 import 'package:meetup/view/auth/frequency_screen.dart';
 import 'package:meetup/view/auth/info_screen.dart';
 import 'package:meetup/view/auth/interest_screen.dart';
 import 'package:meetup/view/auth/login_screen.dart';
-import 'package:meetup/view/auth/webKakaoLogin.dart';
 import 'package:meetup/view/auth/welcome_screen.dart';
 import 'package:meetup/view/home/home_screen.dart';
 import 'package:meetup/view/home/news_letter_screen.dart';
@@ -24,10 +22,6 @@ class Pages {
     GetPage(
       name: Routes.LOGIN,
       page: () => LoginScreen(),
-    ),
-    GetPage(
-      name: Routes.KAKAOLOGIN,
-      page: () => WebKakaoLogin(),
     ),
     GetPage(
       name: Routes.NICKNAME,
