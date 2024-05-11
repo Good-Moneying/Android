1c9fc569a9245e17453483b0fdf557ad7112b6c2
gitsuhyun <suhyun10201@naver.com>
2024-05-11 13:47:24 +0900

    [Feat] 에디터 뉴스 카드로 화면 이동

--- lib/view/home/home_screen.dart
+++ lib/view/home/home_screen.dart
@@ -14,7 +14,6 @@ import '../../design/style/ColorStyles.dart';
 import '../../design/style/FontStyles.dart';
 import '../../design/widgets/tooltip_balloon.dart';
 import '../../routes/get_pages.dart';
-import 'news_letter_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -66,75 +65,80 @@ class _HomeScreenState extends State<HomeScreen> {
                 height: Get.height * 0.02,
               ),
               //에디터 카드 위젯 만들기
-              Card(
-                color: AppColors.white,
-                surfaceTintColor: Colors.transparent,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12)),
-                elevation: 3,
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Stack(
-                        children: [
-                          ClipRRect(
-                            borderRadius: const BorderRadius.all(
-                              Radius.circular(12),
+              InkWell(
+                onTap: () {
+                  Get.toNamed(Routes.EDITORNEWS);
+                },
+                child: Card(
+                  color: AppColors.white,
+                  surfaceTintColor: Colors.transparent,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12)),
+                  elevation: 3,
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: const BorderRadius.all(
+                                Radius.circular(12),
+                              ),
+                              //임시 사진
+                              child: Image.network(
+                                'https://cdn.pixabay.com/photo/2016/03/23/15/00/ice-cream-1274894_1280.jpg',
+                                height: Get.height * 0.25,
+                                fit: BoxFit.fill,
+                              ),
                             ),
-                            //임시 사진
-                            child: Image.network(
-                              'https://cdn.pixabay.com/photo/2016/03/23/15/00/ice-cream-1274894_1280.jpg',
-                              height: Get.height * 0.25,
-                              fit: BoxFit.fill,
-                            ),
-                          ),
-                          Positioned(
-                            top: 13,
-                            right: 16,
-                            child: Stack(
-                              alignment: Alignment.center,
-                              children: [
-                                Container(
-                                  width: 34,
-                                  height: 34,
-                                  decoration: BoxDecoration(
-                                    color: AppColors.white.withOpacity(0.5),
-                                    borderRadius:
-                                        BorderRadius.all(Radius.circular(6)),
+                            Positioned(
+                              top: 13,
+                              right: 16,
+                              child: Stack(
+                                alignment: Alignment.center,
+                                children: [
+                                  Container(
+                                    width: 34,
+                                    height: 34,
+                                    decoration: BoxDecoration(
+                                      color: AppColors.white.withOpacity(0.5),
+                                      borderRadius:
+                                          BorderRadius.all(Radius.circular(6)),
+                                    ),
                                   ),
-                                ),
-                                SvgPicture.asset(
-                                    'assets/icons/bookmark_unfill.svg'),
-                              ],
+                                  SvgPicture.asset(
+                                      'assets/icons/bookmark_unfill.svg'),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: Get.height * 0.03,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 5.0),
-                        child: Text(
-                          '테슬라 주가 갑자기 오른 이유는?',
-                          style: FontStyles.Headline2_b.copyWith(
-                              color: AppColors.black),
+                          ],
                         ),
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          CustomChip(label: '미국경제'),
-                          CustomChip(label: '금리'),
-                          Padding(
-                            padding: const EdgeInsets.only(top: 8.0, left: 10),
-                            child: History(),
+                        SizedBox(
+                          height: Get.height * 0.03,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 5.0),
+                          child: Text(
+                            '테슬라 주가 갑자기 오른 이유는?',
+                            style: FontStyles.Headline2_b.copyWith(
+                                color: AppColors.black),
                           ),
-                        ],
-                      ),
-                    ],
+                        ),
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            CustomChip(label: '미국경제'),
+                            CustomChip(label: '금리'),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 8.0, left: 10),
+                              child: History(),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
