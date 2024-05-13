38839f0421857731ae16b0d1cb64eaadfdfec236
jeong724 <oliveyou724@gmail.com>
2024-05-14 00:43:32 +0900

    [feat]

--- lib/main.dart
+++ lib/main.dart
@@ -50,7 +50,7 @@ class MyApp extends StatelessWidget {
       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       //   useMaterial3: true,
       // ),
-      home: ProfileScreen(),
+      home: LoginScreen(),
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
