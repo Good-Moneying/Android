d39119310b79c0565989249ec09299bb6cf3f5e8
jeong724 <oliveyou724@gmail.com>
2024-05-13 20:04:28 +0900

    [feat] 팝업창 아이콘 버튼 ui 변경

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,7 @@ class MyApp extends StatelessWidget {
       //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       //   useMaterial3: true,
       // ),
-      home: LoginScreen(),
+      home: BottomNavigationView(),
       localizationsDelegates: [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
