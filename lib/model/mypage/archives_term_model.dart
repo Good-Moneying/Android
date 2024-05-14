a2f3ae7a2311c66623571ef97bf4ccb70e31ea17
jeong724 <oliveyou724@gmail.com>
2024-05-14 18:26:57 +0900

    [feat] 마이페이지 아카이브(단어) test api 모델 클래스

--- lib/model/mypage/archives_term_model.dart
+++ lib/model/mypage/archives_term_model.dart
@@ -1,15 +1,44 @@
 class ArchivesTermModel {
-  List<int>? ids;
+  List<Terms>? terms;
 
-  ArchivesTermModel({this.ids});
+  ArchivesTermModel({this.terms});
 
   ArchivesTermModel.fromJson(Map<String, dynamic> json) {
-    ids = json['ids'].cast<int>();
+    if (json['terms'] != null) {
+      terms = <Terms>[];
+      json['terms'].forEach((v) {
+        terms!.add(new Terms.fromJson(v));
+      });
+    }
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['ids'] = this.ids;
+    if (this.terms != null) {
+      data['terms'] = this.terms!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class Terms {
+  int? id;
+  String? koreanName;
+  String? englishName;
+
+  Terms({this.id, this.koreanName, this.englishName});
+
+  Terms.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    koreanName = json['koreanName'];
+    englishName = json['englishName'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['koreanName'] = this.koreanName;
+    data['englishName'] = this.englishName;
     return data;
   }
 }
