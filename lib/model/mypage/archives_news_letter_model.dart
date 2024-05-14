f2a60e5adb359f9e0d1003f836ff64952b32be0f
jeong724 <oliveyou724@gmail.com>
2024-05-14 19:10:49 +0900

    [feat] 마이페이지 아카이브(뉴스레터) test api 레포지토리 클래스

--- lib/model/mypage/archives_news_letter_model.dart
+++ lib/model/mypage/archives_news_letter_model.dart
@@ -1,15 +1,47 @@
 class ArchivesNewsLetterModel {
-  List<int>? ids;
+  List<NewsLetters>? newsLetters;
 
-  ArchivesNewsLetterModel({this.ids});
+  ArchivesNewsLetterModel({this.newsLetters});
 
   ArchivesNewsLetterModel.fromJson(Map<String, dynamic> json) {
-    ids = json['ids'].cast<int>();
+    if (json['newsLetters'] != null) {
+      newsLetters = <NewsLetters>[];
+      json['newsLetters'].forEach((v) {
+        newsLetters!.add(new NewsLetters.fromJson(v));
+      });
+    }
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['ids'] = this.ids;
+    if (this.newsLetters != null) {
+      data['newsLetters'] = this.newsLetters!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
+}
+
+class NewsLetters {
+  int? id;
+  String? title;
+  String? category;
+  String? createdAt;
+
+  NewsLetters({this.id, this.title, this.category, this.createdAt});
+
+  NewsLetters.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    category = json['category'];
+    createdAt = json['createdAt'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['title'] = this.title;
+    data['category'] = this.category;
+    data['createdAt'] = this.createdAt;
     return data;
   }
 }
