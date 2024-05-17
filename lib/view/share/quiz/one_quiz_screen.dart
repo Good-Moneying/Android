4a8910ec472e08dab7278fb98db7521944b3bc76
gitsuhyun <suhyun10201@naver.com>
2024-05-17 22:22:51 +0900

    [Feat] 힌트 다이얼로그 위젯화

--- lib/view/share/quiz/one_quiz_screen.dart
+++ lib/view/share/quiz/one_quiz_screen.dart
@@ -9,6 +9,7 @@ import 'package:meetup/design/widgets/appBar/back_appBar.dart';
 import 'package:meetup/design/widgets/custom_button.dart';
 import 'package:meetup/design/widgets/progress_bar.dart';
 import 'package:meetup/design/widgets/share/choice_quiz.dart';
+import 'package:meetup/design/widgets/share/hint_dialog.dart';
 
 import '../../../routes/get_pages.dart';
 
@@ -59,160 +60,17 @@ class OneQuizScreen extends StatelessWidget {
                       label: '힌트 보기',
                       onPressed: () {
                         showDialog(
-                            barrierDismissible: false,
-                            context: context,
-                            builder: (BuildContext context) {
-                              return StatefulBuilder(builder:
+                          barrierDismissible: false,
+                          context: context,
+                          builder: (BuildContext context) {
+                            return StatefulBuilder(
+                              builder:
                                   (BuildContext context, StateSetter setState) {
-                                return AlertDialog(
-                                  surfaceTintColor: Colors.transparent,
-                                  backgroundColor: AppColors.white,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(12),
-                                  ),
-                                  //insetPadding: EdgeInsets.zero,
-                                  contentPadding: const EdgeInsets.only(
-                                      left: 16.0, right: 16.0, bottom: 32),
-                                  title: Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: [
-                                      Image.asset(
-                                        'assets/icons/mypage_rain.png',
-                                        width: 29,
-                                        height: 31,
-                                      ),
-                                      Text(
-                                        "3 토큰",
-                                        style: FontStyles.Headline2_b.copyWith(
-                                            color: AppColors.v6),
-                                      ),
-                                    ],
-                                  ),
-                                  content: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(top: 19.0, bottom: 8.0),
-                                        child: Text(
-                                          '토큰을 사용해서 힌트를 보시겠어요?',
-                                          style:
-                                              FontStyles.Headline2_b.copyWith(
-                                                  color: AppColors.black),
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(bottom: 17.0),
-                                        child: Text(
-                                          '3 토큰을 사용하시면 힌트를 볼 수 있어요!',
-                                          style: FontStyles.Caption1_m.copyWith(
-                                              color: AppColors.g4),
-                                        ),
-                                      ),
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(bottom: 19.0),
-                                        child: Container(
-                                          decoration: BoxDecoration(
-                                            color: AppColors.g1,
-                                            borderRadius:
-                                                BorderRadius.circular(6),
-                                          ),
-                                          child: Row(
-                                            children: [
-                                              Padding(
-                                                padding:
-                                                    const EdgeInsets.fromLTRB(
-                                                        24, 14, 13, 14),
-                                                child: Text(
-                                                  '현재 보유 토큰',
-                                                  style:
-                                                      FontStyles.Ln1_m.copyWith(
-                                                          color:
-                                                              AppColors.black),
-                                                ),
-                                              ),
-                                              Spacer(),
-                                              Image.asset(
-                                                'assets/icons/mypage_rain.png',
-                                                width: 29,
-                                                height: 31,
-                                              ),
-                                              Padding(
-                                                padding: const EdgeInsets.only(
-                                                    right: 13.0),
-                                                child: Text(
-                                                  '20',
-                                                  style:
-                                                      FontStyles.Ln1_m.copyWith(
-                                                          color:
-                                                              AppColors.black),
-                                                ),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                      Row(
-                                        children: [
-                                          Flexible(
-                                            flex: 1,
-                                            child: OutlinedButton(
-                                              onPressed: () {},
-                                              style: OutlinedButton.styleFrom(
-                                                minimumSize:
-                                                    const Size.fromHeight(50),
-                                                backgroundColor:
-                                                    AppColors.white,
-                                                side: BorderSide(
-                                                  color: AppColors.g3,
-                                                ),
-                                                shape: RoundedRectangleBorder(
-                                                  borderRadius:
-                                                      BorderRadius.circular(8),
-                                                ),
-                                              ),
-                                              child: Text(
-                                                '안볼래요',
-                                                style:
-                                                    FontStyles.Ln1_sb.copyWith(
-                                                  color: Color(0xFFAAAAB9),
-                                                ),
-                                              ),
-                                            ),
-                                          ),
-                                          SizedBox(
-                                            width: 12,
-                                          ),
-                                          Flexible(
-                                            flex: 1,
-                                            child: ElevatedButton(
-                                              onPressed: () {},
-                                              style: ElevatedButton.styleFrom(
-                                                  minimumSize:
-                                                      const Size.fromHeight(50),
-                                                  backgroundColor: AppColors.g6,
-                                                  shape: RoundedRectangleBorder(
-                                                    borderRadius:
-                                                        BorderRadius.circular(
-                                                            8),
-                                                  )),
-                                              child: Text(
-                                                '힌트 볼래요',
-                                                style:
-                                                    FontStyles.Ln1_sb.copyWith(
-                                                        color: AppColors.white),
-                                              ),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ],
-                                  ),
-                                );
-                              });
-                            });
+                                return HintDialog();
+                              },
+                            );
+                          },
+                        );
                       },
                     ),
                   ),
