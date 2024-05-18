293845ebb964eead9afdada901f304d6fced5547
gitsuhyun <suhyun10201@naver.com>
2024-05-19 00:43:56 +0900

    [Feat] 단어 다이얼로그 위젯화

--- lib/view/home/today_term_screen.dart
+++ lib/view/home/today_term_screen.dart
@@ -5,6 +5,7 @@ import 'package:meetup/design/widgets/appBar/back_appBar.dart';
 import 'package:meetup/design/widgets/chip_editor.dart';
 import 'package:meetup/design/widgets/custom_button.dart';
 import 'package:meetup/design/widgets/home/recommend_box.dart';
+import 'package:meetup/design/widgets/home/term_dialog.dart';
 
 import '../../design/style/ColorStyles.dart';
 import 'package:get/get.dart';
@@ -64,69 +65,7 @@ class TodayTermScreen extends StatelessWidget {
                             return StatefulBuilder(
                               builder:
                                   (BuildContext context, StateSetter setState) {
-                                return AlertDialog(
-                                  surfaceTintColor: Colors.transparent,
-                                  backgroundColor: AppColors.white,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(12),
-                                  ),
-                                  titlePadding: EdgeInsets.only(right: 16.0),
-                                  contentPadding: EdgeInsets.fromLTRB(16, 16, 16, 32),
-                                  title: Row(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      IconButton(
-                                        onPressed: () {
-                                          Get.back();
-                                        },
-                                        icon: SvgPicture.asset(
-                                            'assets/images/newsletter_close.svg'), // 닫기 아이콘
-                                      ),
-                                    ],
-                                  ),
-                                  content: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Padding(
-                                        padding: const EdgeInsets.only(bottom: 8.0),
-                                        child: RichText(
-                                          text: TextSpan(
-                                            text: '오늘의 단어를 ',
-                                            style: FontStyles.Headline1_b.copyWith(color: AppColors.black),
-                                            children: <TextSpan>[
-                                              TextSpan(
-                                                text: '저장',
-                                                style: FontStyles.Headline1_b.copyWith(
-                                                    color: AppColors.v5),
-                                              ),
-                                              TextSpan(
-                                                text: '했어요!',
-                                                style: FontStyles.Headline1_b.copyWith(
-                                                    color: AppColors.black),
-                                              ),
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                      Text('마이페이지 > 나의 단어장에서 확인해보세요.',
-                                      style: FontStyles.Caption1_m.copyWith(color: AppColors.g4),
-                                      ),
-                                      Padding(
-                                        padding: const EdgeInsets.only(bottom: 32.0),
-                                        child: Image.asset('assets/icons/plus_cloudfun.png'),
-                                      ),
-                                      CustomButton(
-                                          backgroundColor: AppColors.v6,
-                                          textStyle: FontStyles.Ln1_sb.copyWith(color: AppColors.white),
-                                          label: '나의 단어장 확인하기',
-                                          onPressed: () {
-                                            //네비바
-                                            Get.toNamed(Routes.PROFILE);
-                                          },
-                                      ),
-                                    ],
-                                  ),
-                                );
+                                return TermDialog();
                               },
                             );
                           },
