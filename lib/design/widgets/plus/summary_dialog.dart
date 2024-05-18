4169ee724a37dc742bf71cabf92dc70b185beec6
jeong724 <oliveyou724@gmail.com>
2024-05-18 16:14:17 +0900

    [feat] 생각더하기 다이얼로그 widget ui

---
+++ lib/design/widgets/plus/summary_dialog.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../style/ColorStyles.dart';
+import '../../style/FontStyles.dart';
+
+class SummaryDialog extends StatelessWidget {
+  const SummaryDialog({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      surfaceTintColor: Colors.transparent,
+      backgroundColor: AppColors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius:
+        BorderRadius.circular(13.2),
+      ),
+      title: Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children :[
+            SvgPicture.asset('assets/icons/plus_close.svg')
+          ]
+      ),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          RichText(
+            text: TextSpan(
+              text: '작성하고 ',
+              style: FontStyles.Headline1_b.copyWith(
+                  color: AppColors.black), //기본style을 지정해줘야함
+              children: <TextSpan>[
+                TextSpan(
+                  text: '‘요약하기’'
+                  ,style: FontStyles.Headline1_b.copyWith(
+                    color: AppColors.v6), //기본style을 지정해줘야함
+                ),
+                TextSpan(
+                  text: '를 눌러주세요'
+                  ,style: FontStyles.Headline1_b.copyWith(
+                    color: AppColors.black), //기본style을 지정해줘야함
+                ),
+              ],
+            ),
+          ),
+          Text(
+            "다 쓰고 요약만 하면 바로 등록할 수 있어요.",
+            style:
+            FontStyles.Caption1_m.copyWith(
+                color: AppColors.g4),
+          ),
+          Image.asset('assets/icons/plus_cloud.png'),
+        ],
+      ),
+    );
+  }
+}
--- lib/view/plus/plus_step1_screen.dart
+++ lib/view/plus/plus_step1_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:meetup/design/widgets/plus/stop_dialog.dart';
+import 'package:meetup/design/widgets/plus/summary_dialog.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 import '../../design/style/ColorStyles.dart';
@@ -384,50 +386,7 @@ class PlusStep1Screen extends StatelessWidget {
                                   return StatefulBuilder(builder:
                                       (BuildContext context,
                                           StateSetter setState) {
-                                    return AlertDialog(
-                                      surfaceTintColor: Colors.transparent,
-                                      backgroundColor: AppColors.white,
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(13.2),
-                                      ),
-                                      title: Text(
-                                        "아직 생각 구름이 만들어지지 않았어요!",
-                                        style: FontStyles.Headline1_b.copyWith(
-                                            color: AppColors.black),
-                                      ),
-                                      content: Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          Text(
-                                            "정말 그만두시겠어요? 조금만 더 하면 완성해요!",
-                                            style:
-                                                FontStyles.Caption1_m.copyWith(
-                                                    color: AppColors.g4),
-                                          ),
-                                          Image.asset('assets/icons/plus_dialog_char.png'),
-                                          Row(
-                                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                            children: [
-                                              Container(
-                                                width: 114, height: 48
-                                                ,decoration: BoxDecoration(
-                                                  borderRadius: BorderRadius.circular(8), border: Border.all(color: AppColors.g2)
-                                                ),
-                                                child: Center(child: Text('그만하기', style: FontStyles.Ln1_sb.copyWith(color: AppColors.g4),)),
-                                              ),
-                                              Container(
-                                                width: 167, height: 48
-                                                ,decoration: BoxDecoration(
-                                                  borderRadius: BorderRadius.circular(8), color: AppColors.v6
-                                              ),
-                                                child: Center(child: Text('구름 만들기', style: FontStyles.Ln1_sb.copyWith(color: AppColors.white),)),
-                                              ),
-                                            ],
-                                          )
-                                        ],
-                                      ),
-                                    );
+                                    return StopDialong();
                                   });
                                 }));
                           },
