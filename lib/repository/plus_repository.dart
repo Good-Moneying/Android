fed3ba7d655aa0cddf3eb8e0350882a63c9a4fb5
jeong724 <oliveyou724@gmail.com>
2024-05-20 15:53:38 +0900

    [add] 생각 더하기 홈 레포지토리 추가

---
+++ lib/repository/plus_repository.dart
@@ -0,0 +1,32 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import '../model/plus/cloud_home_medel.dart';
+
+class PlusRepository {
+  final Dio _dio = Dio();
+
+  ProfileRepository() {
+    _dio.options.baseUrl = dotenv.get("BASE_URL");
+    _dio.options.validateStatus = (status) {
+      return status! < 500;
+    };
+  }
+
+  Future<CloudHomeModel> cloudHomeGet() async {
+    try {
+      print('cloudHomeGet() 호출');
+      final response = await _dio.get("/api/thinkings/home");
+
+      if (response.statusCode == 200) {
+        return CloudHomeModel.fromJson(response.data);
+      } else {
+        throw Exception(
+            'cloudHomeGet 실패! : ${response.statusCode} - ${response.statusMessage}');
+      }
+    } catch (e) {
+      print('오류 발생!: $e');
+      throw Exception('Error occurred!!: $e');
+    }
+  }
+}
