c8ebb11fb1a0bc4cf661f7d3ebf2fd6801007eb5
gitsuhyun <suhyun10201@naver.com>
2024-05-22 17:26:53 +0900

    [Add] 설문조사용 박스 위젯 추가

--- lib/design/widgets/share/survey_box.dart
+++ lib/design/widgets/share/survey_box.dart
@@ -3,26 +3,23 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import '../history_widget.dart';
 import '../../style/ColorStyles.dart';
 import '../../style/FontStyles.dart';
+import '../chip_editor.dart';
 
-class RecommendU extends StatelessWidget {
+class SurveyBox extends StatelessWidget {
   final String image;
   final String title;
   final Widget tag;
   final Widget history;
-  final bool isRecommend;
-  final Function() onRecommend;
 
-
-  const RecommendU({
+  const SurveyBox({
     super.key,
     required this.image,
     required this.title,
     required this.tag,
     required this.history,
-    required this.isRecommend,
-    required this.onRecommend,
   });
 
   @override
@@ -33,7 +30,7 @@ class RecommendU extends StatelessWidget {
           color: AppColors.white,
           surfaceTintColor: Colors.transparent,
           shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(14)),
           child: Padding(
             padding: const EdgeInsets.fromLTRB(6, 6, 12, 6),
             child: Row(
@@ -59,32 +56,6 @@ class RecommendU extends StatelessWidget {
                       title,
                       style: FontStyles.Ln1_m.copyWith(color: AppColors.black),
                       softWrap: true,
-                    ),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: onRecommend,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 6.0),
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        Container(
-                          width: 34,
-                          height: 34,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(6)),
-                            border: Border.all(
-                              color: AppColors.g2.withOpacity(0.3),
-                            ),
-                          ),
-                        ),
-                        SvgPicture.asset(
-                            isRecommend
-                                ? 'assets/icons/bookmark_fill.svg'
-                                : 'assets/icons/bookmark_unfill.svg'
-                        ),
-                      ],
                     ),
                   ),
                 ),
